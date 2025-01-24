4bdfbab72036f251a46faa672b5c6c8f9c1bfd6d
Victor Debaza <victor.debaza@gmail.com>
2025-01-24 16:02:18 +0000

    Update .gitignore to include reports directory and modify Jest config to generate timestamped test report filenames

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "jest-html-reporter",
         {
           pageTitle: "Test Report",
-          outputPath: "./reports/test-report.html",
+          outputPath: `./reports/test-report-${new Date().toISOString().replace(/:/g, '-')}.html`,
           includeFailureMsg: true,
           includeConsoleLog: true
         }
